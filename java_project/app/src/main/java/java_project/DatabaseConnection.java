ddaa35de88db022adebf921486635612899d7d78
faresUnderScore999 <benalifares999@gmail.com>
2026-01-21 07:40:46 +0100

    Initial commit: JavaFX + JDBC project

---
+++ java_project/app/src/main/java/java_project/DatabaseConnection.java
@@ -0,0 +1,28 @@
+package main.java.java_project;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private Connection connection;
+
+    private final String URL = "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7814865?useSSL=false&serverTimezone=UTC";
+    private final String USER = "sql7814865";
+    private final String PASSWORD = "8DDgEIrFGJ";
+
+    private DatabaseConnection() throws SQLException {
+        connection = DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+}
